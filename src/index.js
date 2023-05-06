ea0eaa8b80dded3890079d6bc1da2f266c912482
Everton Martins <everton.martins@omnilink.com.br>
2023-05-06 15:08:01 -0300

    Identificando links

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}));
+
+    return resultados.length !== 0 ? resultados : 'não há links no arquivo';
+}
+
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
-// Async/Await]
 async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(chalk.green(texto));
+        return extraiLinks(texto);
     } catch (erro) {
         trataErro(erro);
-    } finally {
-        console.log(chalk.yellow('operação concluída'));
     }
 }
 
-pegaArquivo('./arquivos/texto.md');
+export default pegaArquivo;
